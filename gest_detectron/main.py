966152c9525d4ad862567e8dc78f5653de88a65b
therealnaveenkamal@gmail.com <therealnaveenkamal@gmail.com>
2025-01-24 22:55:16 -0500

    Added Notebook, Sample Assets, Removed Versions from requirements due to conflicts, added final output video

--- gest_detectron/main.py
+++ gest_detectron/main.py
@@ -38,17 +38,17 @@ else:
 print(f"using device: {device}")
 
 
-# if device.type == "cuda":
-#     torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
-#     if torch.cuda.get_device_properties(0).major >= 8:
-#         torch.backends.cuda.matmul.allow_tf32 = True
-#         torch.backends.cudnn.allow_tf32 = True
-# elif device.type == "mps":
-#     print(
-#         "\nSupport for MPS devices is preliminary. SAM 2 is trained with CUDA and might "
-#         "give numerically different outputs and sometimes degraded performance on MPS. "
-#         "See e.g. https://github.com/pytorch/pytorch/issues/84936 for a discussion."
-#     )
+if device.type == "cuda":
+    torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+    if torch.cuda.get_device_properties(0).major >= 8:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+elif device.type == "mps":
+    print(
+        "\nSupport for MPS devices is preliminary. SAM 2 is trained with CUDA and might "
+        "give numerically different outputs and sometimes degraded performance on MPS. "
+        "See e.g. https://github.com/pytorch/pytorch/issues/84936 for a discussion."
+    )
 
 print("Predictor Calling")
 
